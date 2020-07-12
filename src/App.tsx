d663a7aa106ccf0f118e00711d3d3b029eef4a69
Annie Huang <annie.hn@gmail.com>
2020-07-12 14:23:55 +1000

    Reformat file

--- src/App.tsx
+++ src/App.tsx
@@ -5,24 +5,22 @@ const numRows = 50;
 const numCols = 50;
 
 const App: React.FC = () => {
-    // I didn't know we can set function inside useState like this?
-    const [grid, setGrid] = useState(() => {
-        const rows = [];
+  // I didn't know we can set function inside useState like this...
+  const [grid, setGrid] = useState(() => {
+    const rows = [];
 
-        // 0: dead. 1: life.
-        for(let i = 0; i<numRows; i++) {
-            // you can also do Array.from(Array(numCols), ()=>0) )
-            rows.push(Array(numCols).fill(0));
-        }
+    // 0: dead. 1: life.
+    for (let i = 0; i < numRows; i++) {
+      // you can also do Array.from(Array(numCols), ()=>0) )
+      rows.push(Array(numCols).fill(0));
+    }
 
-        return rows;
-    });
+    return rows;
+  });
 
-    console.log(grid);
+  // console.log(grid);
 
-    return (
-        <div>yo</div>
-    );
+
 }
 
 export default App;
