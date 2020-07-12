bcaad687ca1cd5a959e70d8729caae743d94d1af
Annie Huang <annie.hn@gmail.com>
2020-07-12 13:03:54 +1000

    Created a 50x50 grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 
+const numRows = 50;
+const numCols = 50;
+
 const App: React.FC = () => {
-  return (
-    <div>yo</div>
-  );
+    // I didn't know we can set function inside useState like this?
+    const [grid, setGrid] = useState(() => {
+        const rows = [];
+
+        // 0: dead. 1: life.
+        for(let i = 0; i<numRows; i++) {
+            // you can also do Array.from(Array(numCols), ()=>0) )
+            rows.push(Array(numCols).fill(0));
+        }
+
+        return rows;
+    });
+
+    console.log(grid);
+
+    return (
+        <div>yo</div>
+    );
 }
 
 export default App;
